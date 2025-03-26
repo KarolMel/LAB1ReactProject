4e4a4174b8f6f09b96017221e117e103470f74a7
KaroluMel <k_melnicki@o2.pl>
2025-03-26 08:19:51 +0100

    Can convert now

--- src/components/ConvertResult.tsx
+++ src/components/ConvertResult.tsx
@@ -1,9 +1,23 @@
-const ConvertResult: React.FC = () => {
+import React from "react"
+import "../ConvertResult.css"
+
+interface ConvertResultProps {
+    result: number | null;
+    secondCurrency: string;
+    firstCurrency: string;
+    amount: number;
+}
+
+const ConvertResult: React.FC<ConvertResultProps> = ({result, secondCurrency, firstCurrency, amount}) => {
     return (
         <>
-            <div>
-                <h1>This is convert result</h1>
-            </div>
+        <div className="converted">
+            {result !== null ? (
+                <p>Conversion Result: {`${amount} ${firstCurrency} is ${result} ${secondCurrency}`}</p>
+            ) : (
+                <p>No conversion yet</p>
+            )}
+        </div>
         </>
     )
 }
--- src/components/Convertor.tsx
+++ src/components/Convertor.tsx
@@ -1,21 +1,46 @@
 import React from "react"
 import "../ConvertForm.css"
 import ConvertResult from "./ConvertResult"
+import "../ConvertorCSS.css"
+
+import { useState } from "react"
 
 const Convertor: React.FC = () => {
+
+    const [currencyData, setCurrencyData] = useState<any>(null);
+    const [firstCurrency, setFitstCurrency] = useState<string>('');
+    const [secondCurrency, setSecondCurrency] = useState<string>('');
+    const [amount, setAmount] = useState<number>(0)
+    const [result, setResult] = useState<number | null>(null)
+
+    const apiKey: string = '439863f844222b85c807fc72';
+    const url: string = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${firstCurrency}`;
+
+    function fetchData() {
+        fetch(url)
+        .then((response) => response.json())
+        .then((resultData) => {
+            setCurrencyData(resultData)
+            const rate = resultData.conversion_rates[secondCurrency];
+            const conversionResult = rate * amount;
+            setResult(parseFloat(conversionResult.toFixed(2)))
+        })
+    }
+
+
     return (
         <>
         <div className="convert-form">
          <form>
             <label>First Valute</label>
-                <input type="text" placeholder="Please enter first valute"/>
+                <input value={firstCurrency} type="text" placeholder="Please enter first valute" onChange={(e) => setFitstCurrency(e.target.value)}/>
             <label>Second Value</label>
-                <input type="text" placeholder="Please enter second valute"/>
+                <input value={secondCurrency} type="text" placeholder="Please enter second valute" onChange={(e) => setSecondCurrency(e.target.value)}/>
             <label>How much?</label>
-                <input type="number" placeholder="Please enter how much you want to convert"/>
-            <input type="button" value="Convert valute"/>
+                <input type="number" placeholder="Please enter how much you want to convert" onChange={(e) => setAmount(Number(e.target.value))}/>
+            <input onClick={fetchData} type="button" value="Convert valute"/>
          </form>
-         <ConvertResult />
+         <ConvertResult result={result} secondCurrency={secondCurrency} firstCurrency={firstCurrency} amount={amount} />
          </div>
         </>
     )
